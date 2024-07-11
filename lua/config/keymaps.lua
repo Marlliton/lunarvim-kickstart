466bde7715dd31098aabdf662bf05498e0c6dd23
Marlliton <marlliton.souza1@gmail.com>
2024-07-11 18:50:50 -0300

    Adiconando opções para organização de imports

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,7 @@ local kind = require("config.kind")
 
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<leader>c"] = ":bdelete<CR>"
 
 -- busca referências de uma palavra em arquivo
 lvim.lsp.buffer_mappings.normal_mode["gr"] = {
@@ -9,14 +10,21 @@ lvim.lsp.buffer_mappings.normal_mode["gr"] = {
   kind.cmp_kind.Reference .. " Find references"
 }
 
--- vai para a definição de uma função 
+-- vai para a definição de uma função
 lvim.lsp.buffer_mappings.normal_mode["gd"] = {
   ":lua vim.lsp.buf.definition()<cr>",
   -- ":lua require'telescope.builtin'.lsp_definitions()<cr>",
   kind.cmp_kind.Reference .. " Definitions"
 }
 
--- lvim.lsp.buffer_mappings.normal_mode["<leader>k"] = {
---   vim.lsp.buf.hover,
---   " Show hover information"
--- }
+-- Imports
+lvim.builtin.which_key.mappings["i"] = { name = "Imports" }
+lvim.builtin.which_key.mappings["i"]["o"] = {
+  ":lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })<CR>",
+  kind.cmp_kind.Text .. "Organize Imports"
+}
+lvim.builtin.which_key.mappings["i"]["r"] = {
+  ":lua vim.lsp.buf.code_action({ context = { only = { 'source.removeUnused' } }, apply = true })<CR>",
+  kind.icons.exit .. "Remove Unused Imports"
+}
+
