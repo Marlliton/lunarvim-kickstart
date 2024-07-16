ed476a52bca5226195a63bb7a625b13647417a71
Marlliton <marlliton.souza1@gmail.com>
2024-07-16 20:39:11 -0300

    Telescope keymaps

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,14 +1,3 @@
-lvim.builtin.which_key.mappings["f"] = { name = "Find" }
-lvim.builtin.which_key.mappings["f"]["f"] = { "<cmd>Telescope find_files<cr>", "Find File" }
-lvim.builtin.which_key.mappings["f"]["g"] = { "<cmd>Telescope live_grep<cr>", "Live Grep" }
-lvim.builtin.which_key.mappings["f"]["s"] = { "<cmd>Telescope grep_string<cr>", "Grep String" }
-lvim.builtin.which_key.mappings["f"]["G"] = { "<cmd>Telescope git_files<cr>", "Git Files" }
-lvim.builtin.which_key.mappings["f"]["a"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" }
-
-
-lvim.builtin.which_key.mappings["p"] = { "<cmd>Telescope projects<cr>", "Recent Projects" }
-
--- styles
 
 lvim.builtin.telescope.defaults = {
   -- use fd to "find files" and return absolute paths
--- lua/setup/keymaps/whichkey.lua
+++ lua/setup/keymaps/whichkey.lua
@@ -1,5 +1,5 @@
 local kind = require("config.kind")
--- Imports
+-- Imports start
 lvim.builtin.which_key.mappings["i"] = { name = "Imports" }
 lvim.builtin.which_key.mappings["i"]["o"] = {
   ":lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })<CR>",
@@ -9,3 +9,14 @@ lvim.builtin.which_key.mappings["i"]["r"] = {
   ":lua vim.lsp.buf.code_action({ context = { only = { 'source.removeUnused' } }, apply = true })<CR>",
   kind.icons.exit .. "Remove Unused Imports"
 }
+-- Imports ends
+
+-- Telescope start
+lvim.builtin.which_key.mappings["f"] = { name = "Find" }
+lvim.builtin.which_key.mappings["f"]["f"] = { "<cmd>Telescope find_files<cr>", "Find File" }
+lvim.builtin.which_key.mappings["f"]["g"] = { "<cmd>Telescope live_grep<cr>", "Live Grep" }
+lvim.builtin.which_key.mappings["f"]["s"] = { "<cmd>Telescope grep_string<cr>", "Grep String" }
+lvim.builtin.which_key.mappings["f"]["G"] = { "<cmd>Telescope git_files<cr>", "Git Files" }
+lvim.builtin.which_key.mappings["f"]["a"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" }
+lvim.builtin.which_key.mappings["p"] = { "<cmd>Telescope projects<cr>", "Recent Projects" }
+-- Telescope ends
